6a11a2e979c7f7fa11b14ff38d6746d708bffa9e
Darren Chen <broswithswag@gmail.com>
2018-07-22 14:27:22 -0700

    Main Activity Slider Implemented
    Main screen slide is implemented, images can now be added to suit purposed of application (preview of our app I guess)

--- app/src/main/java/com/gogogatchi/gogogatchi/activities/MainActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/MainActivity.java
@@ -2,24 +2,37 @@ package com.gogogatchi.gogogatchi.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Button;
 
 import com.gogogatchi.gogogatchi.R;
+import com.gogogatchi.gogogatchi.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button=findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MonsterHatchActivity.class);
             startActivity(intent);}
         });
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
 
     }
 }
