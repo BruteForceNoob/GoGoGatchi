3c0bef92dbfaa200224357d8f33a5ead78b340d2
Darren Chen <broswithswag@gmail.com>
2018-07-28 17:44:46 -0700

    Firebase up and running for User Credentials (Login)
    long title yet again. Also changes to id names for buttons and text on main xml

---
+++ app/src/main/java/com/gogogatchi/gogogatchi/FirebaseDB.java
@@ -0,0 +1,20 @@
+package com.gogogatchi.gogogatchi;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//code created for re-usability
+
+
+public class FirebaseDB {
+    public final static FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    public final static DatabaseReference mDatabase =
+            FirebaseDatabase.getInstance().getReference();
+    public static FirebaseAuth getAuthConnection() {
+        return FirebaseAuth.getInstance();
+    }
+    public static DatabaseReference getConnection(){
+        return FirebaseDatabase.getInstance().getReference();
+    }
+}
--- app/src/main/java/com/gogogatchi/gogogatchi/activities/MainActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/MainActivity.java
@@ -2,31 +2,116 @@ package com.gogogatchi.gogogatchi.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.util.Patterns;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import com.gogogatchi.gogogatchi.R;
 import com.gogogatchi.gogogatchi.ViewPagerAdapter;
+import com.gogogatchi.gogogatchi.FirebaseDB;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     ViewPager viewPager;
+    private FirebaseAuth mAuth;
+    EditText editTextEmail, editTextPassword;
+    ProgressBar progressBar;
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Username is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(MainActivity.this, MonsterHatchActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //Initialization of text fields and buttons
+    private void init(){
+        editTextEmail = (EditText) findViewById(R.id.editEmail);
+        editTextPassword = (EditText) findViewById(R.id.editPassword);
+        //Enter more if needed
+        mAuth = FirebaseAuth.getInstance();
+
+        Button btnLogin, btnReg;
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnReg = (Button) findViewById(R.id.btnRegister);
+        btnLogin.setOnClickListener(this);
+        btnReg.setOnClickListener(this);
+    }
+
+    //OnClick creation implementation
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnRegister:
+                userLogin();
+                break;
+            case R.id.btnLogin:
+                userLogin();
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+        /*
         Button button=findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MonsterHatchActivity.class);
             startActivity(intent);}
         });
+        */
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
 
@@ -36,3 +121,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
