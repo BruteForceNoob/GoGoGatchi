023451efc4efced22cdbe5d1004e25b89372a282
ankushbhandare <bhandare_ankush@rediffmail.com>
2018-07-29 17:45:44 -0700

    Added user profile to realtime databsase

--- app/src/main/java/com/gogogatchi/gogogatchi/activities/ProfileActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/ProfileActivity.java
@@ -11,9 +11,13 @@ import android.widget.EditText;
 
 import com.gogogatchi.gogogatchi.FirebaseDB;
 import com.gogogatchi.gogogatchi.R;
+import com.gogogatchi.gogogatchi.core.User;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -23,7 +27,7 @@ public class ProfileActivity extends AppCompatActivity {
     private CheckBox isCheckAth;
     private CheckBox isCheckCul;
     private CheckBox isCheckMonu;
-    private EditText user;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +36,30 @@ public class ProfileActivity extends AppCompatActivity {
 
         mRef = FirebaseDB.mDatabase;
         mSendData= findViewById(R.id.updateProfile);
-        user = (EditText)findViewById(R.id.userId);
+        editText = (EditText)findViewById(R.id.userId);
         isCheckAth = findViewById(R.id.athletics);
         isCheckCul = findViewById(R.id.cultural);
         isCheckMonu = findViewById(R.id.monuments);
         mSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseReference mChildRef= mRef.child("User");
+                User user= new User();
+                
+                user.setUsername(editText.getText().toString());
+                user.setPassword("test123");
+                Map<String,Boolean> interests=user.getInterests();
+                interests.put("Athletics",isCheckAth.isChecked());
+                interests.put("Cultural",isCheckCul.isChecked());
+                interests.put("Monuments",isCheckMonu.isChecked());
+                DatabaseReference mChildRef= mRef.child("User").child(user.getUsername());
+                mChildRef.setValue(user);
+
+
                 //user = (EditText)findViewById(R.id.userId);
-                mChildRef.child("Username").setValue(user.getText().toString());
+           /*     mChildRef.child("Username").setValue(user.getText().toString());
                 mChildRef.child("Athletics").setValue(isCheckAth.isChecked());
                 mChildRef.child("Cultural").setValue(isCheckCul.isChecked());
-                mChildRef.child("Monuments").setValue(isCheckMonu.isChecked());
+                mChildRef.child("Monuments").setValue(isCheckMonu.isChecked());*/
                 /*DatabaseReference mGrandChildRef1= mChildRef.child("Athletics");
                 mGrandChildRef1.setValue(isCheckAth.isChecked());
                 DatabaseReference mGrandChildRef2= mChildRef.child("Cultural");
