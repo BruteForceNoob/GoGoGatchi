69a913d5d663ccb8a17a02af83dda79f570b2563
ankushbhandare <bhandare_ankush@rediffmail.com>
2018-07-29 23:28:38 -0700

    Now username is visible in profile

--- app/src/main/java/com/gogogatchi/gogogatchi/activities/ProfileActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/ProfileActivity.java
@@ -75,22 +75,22 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        mSendData.setOnClickListener(new View.OnClickListener() {
+        /*mSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 User user= new User();
-/*
 
-                /*user.setUsername("Ankush");
-                user.setPassword("test123");*/
+
+                user.setUsername("Ankush");
+                user.setPassword("test123");
                 Map<String,Boolean> interests=user.getInterests();
                 interests.put("Athletics",isCheckAth.isChecked());
                 interests.put("Cultural",isCheckCul.isChecked());
                 interests.put("Monuments",isCheckMonu.isChecked());
                 //mChildRef= mRef.child("User").child(user.getUsername());
-                //mChildRef.setValue(user);*/
+                //mChildRef.setValue(user);
             }
-        });
+        });*/
 
         }
 
