cfc902048250d854f0eada3772b1f2a214e4bc9a
ankushbhandare <bhandare_ankush@rediffmail.com>
2018-08-05 19:40:17 -0700

    Profile picture can be uploaded from Gallery. The image will get compressed into a square if it is bigger in size.

--- app/src/main/java/com/gogogatchi/gogogatchi/activities/UpdateProfileActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/UpdateProfileActivity.java
@@ -1,15 +1,62 @@
 package com.gogogatchi.gogogatchi.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.gogogatchi.gogogatchi.R;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class UpdateProfileActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+
+        Button buttonLoadImage = (Button) findViewById(R.id.btnUpdateProfileImage);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), RESULT_LOAD_IMAGE);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    //Detects request codes
+    if(requestCode==RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK) {
+        Uri selectedImage = data.getData();
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            //int nh = (int) ( bitmap.getHeight() * (125.0 / bitmap.getWidth()) );
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 512, 512, true);
+            ImageView imageView = (ImageView) findViewById(R.id.profileImageView);
+            imageView.setImageBitmap(scaled);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 }
