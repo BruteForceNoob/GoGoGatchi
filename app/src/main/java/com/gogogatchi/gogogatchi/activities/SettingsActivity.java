a558dd410249f9fb4975d84d8191624ec5eae141
ankushbhandare <bhandare_ankush@rediffmail.com>
2018-08-05 18:08:26 -0700

    Added new Activity for Updating Profile Added buttons in Settings

--- app/src/main/java/com/gogogatchi/gogogatchi/activities/SettingsActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.gogogatchi.gogogatchi.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.gogogatchi.gogogatchi.R;
 
@@ -11,5 +13,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void updateUserProfile(View view) {
+        Intent intent = new Intent (getApplicationContext(), UpdateProfileActivity.class);
+        startActivity(intent);
     }
 }
