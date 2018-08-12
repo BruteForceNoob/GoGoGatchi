3c3a28800ee41fcecc43f019ce7c9742ef26ce38
ankushbhandare <bhandare_ankush@rediffmail.com>
2018-08-12 16:28:43 -0700

    Added Slider for range 1 to 100 miles Default is at 5 miles

--- app/src/main/java/com/gogogatchi/gogogatchi/activities/SettingsActivity.java
+++ app/src/main/java/com/gogogatchi/gogogatchi/activities/SettingsActivity.java
@@ -5,17 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.gogogatchi.gogogatchi.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    TextView distance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle(getString(R.string.settings));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_settings);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        Integer progress = seekBar.getProgress();
+        distance = findViewById(R.id.txtDistance);
+        distance.setText(progress.toString());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                String progress = ((Integer)i).toString();
+                distance.setText(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
